e5f7e4375649e4d31596130853f6b3d7cab80f72
Krzysztof Magiera <krzys.magiera@gmail.com>
2016-03-22 23:32:47 +0100

    Move all the native animated code to com.facebook.react.animation package.

--- ReactAndroid/src/main/java/com/facebook/react/animated/AnimatedNode.java
+++ ReactAndroid/src/main/java/com/facebook/react/animated/AnimatedNode.java
@@ -7,7 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-package com.facebook.react.uimanager.animation;
+package com.facebook.react.animated;
 
 import com.facebook.infer.annotation.Assertions;
 
